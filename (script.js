708920ba377e9d7080092b1fedd592c23d231773
jbompaturay <jbompaturay@gmail.com>
2025-06-20 07:16:25 -0700

    Add files via upload

---
+++ (script.js
@@ -0,0 +1,9 @@
+// Show welcome message on page load
+window.addEventListener('DOMContentLoaded', () => {
+    alert("Welcome to Jeneba Magdalene Bompa-Turay's Portfolio Website!");
+});
+
+// Toggle dark mode
+function toggleDarkMode() {
+    document.body.classList.toggle('dark-mode');
+}
